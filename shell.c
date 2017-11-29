ea652b8db079d995b2e03b6b8d25c71efe8d46d5
kchen29 <kchen29@stuy.edu>
2017-11-28 19:10:26 -0500

    refactor

--- shell.c
+++ shell.c
@@ -5,6 +5,22 @@
 #include <sys/wait.h>
 
 /*
+  wrapper for fgets
+
+  gets size characters from stdin
+  exit if eof character is the only character inputted
+  remove newline from fgets
+ */
+void get_input(char *s, int size) {
+  //fgets returns null when end of file character occurs
+  //while no characters have been read
+  if (!fgets(s, size, stdin))
+    exit(0);
+  //remove newline
+  *(strchr(s, '\n')) = 0;
+}
+
+/*
   ex: takes "ls -a -l"
   returns array ["ls", "-a", "-l", NULL]
 
@@ -12,7 +28,6 @@
   assume no more than 19 args
   assume correctly formatted
 */
-
 char **parse_args(char *line) {
   //pointer to "array"; free this later
   char **args = malloc(20 * sizeof(char *));
@@ -28,32 +43,40 @@ char **parse_args(char *line) {
   return args;
 }
 
+/*
+  runs one command
+  
+  ex: takes ["ls", "-a", "-l", NULL]
+  and forks and execs it
+ */
+void run_command(char **args) {
+  //special case exit and cd
+  if (strcmp(args[0], "exit") == 0) {
+    exit(0);
+  } else if (strcmp(args[0], "cd") == 0) {
+    chdir(args[1]);
+    return;
+  }
+
+  //for other commands, fork and exec
+  if (fork() == 0) {
+    execvp(args[0], args);
+  } else {
+    int status;
+    wait(&status);
+    //do something here?
+  }
+}
+
 int main() {
   char s[256];
   while(1){
-    //get data
-    //fgets returns null when end of file character occurs while no characters have been read
-    if (!fgets(s, sizeof(s), stdin))
-      return 0;
-    //remove newline from fgets
-    *(strchr(s, '\n')) = 0;
-    
-    //parse
+    get_input(s, sizeof(s));
+
     char **args = parse_args(s);
-    
-    if (strcmp(args[0], "exit") == 0) {
-      return 0;
-    } else if (strcmp(args[0], "cd") == 0) {
-      chdir(args[1]);
-    } else {
-      if (fork() == 0) {
-	execvp(args[0], args);
-      } else {
-	int status;
-	wait(&status);
-	free(args);
-      }
-    }
+
+    run_command(args);
+    free(args);
   }
   return 0;
 }
